1c3f598f1ac6294e63484bae939c4fb0332a5ccd
arvis777 <naujauas001@gmail.com>
2020-10-13 10:25:41 +0300

    fn refresh

--- index.php
+++ index.php
@@ -5,18 +5,16 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>As valdau HTML <?php print "ir PHP" ?></title>
+    <title>As valdau HTML <?php print 'ir PHP'; ?></title>
     <link rel="stylesheet" href="style.css">
 </head>
     <body>
         <h1>
-            <b>Arvydas</b> - HTML <?php print "ir PHP" ?> asas
+            <b>Arvydas</b> - HTML <?php print 'ir PHP'; ?> asas
         </h1>
-            <p>Kuriu dinamini turini.</p>
+            <p>Kuriu dinamini turini.<br/>
+                Atidaryta: <?php print date('Y-m-d h:i:s'); ?>
+            </p>
     </body>
-
-<?php
-print "Atidaryta:" .date("Y-m-d h:i:s");
-?>
 </html>
 
