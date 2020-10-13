31f1699b0a5ba311df96ced85fa3a49127eef244
arvis777 <naujauas001@gmail.com>
2020-10-13 11:26:45 +0300

    date php

--- index.php
+++ index.php
@@ -6,17 +6,15 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>As <?php print date('l') . ' ir PHP'; ?></title>
-    <link rel="stylesheet" href="style.css">
+    <title>PHP lydes ir <?php print date('Y-m-d', strtotime('+1 day')) . '!'; ?></title>
 </head>
     <body>
         <h1>
-            <b>Arvydas</b> - HTML <?php print 'ir PHP'; ?> asas jau nuo <?php print date('Y') . ' metu' ?>
+            <b>Arvydas</b> - PHP su manim buvo ir
+            <?php print date('H', strtotime('-1 hour')) . ' valanda'; ?>
         </h1>
         <p>
-            Viskas prasidejo<br/>
-            <?php print date('m') . ' menesio, ' . date('d') . ' diena!'; ?>
+            <?php print date('Y', strtotime('+1 year')) . ' ne uz kalnu!'; ?>
         </p>
     </body>
 </html>
-
